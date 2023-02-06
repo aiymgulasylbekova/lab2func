5cfa41bcc6fb278d66779cb0a91906dcb7e37a35
aiymgulasylbekova <aiymgulasylbekova.03@gmail.com>
2023-02-07 04:28:30 +0600

    қарапайым if else шарттарын қолдандым

--- labf.py
+++ labf.py
@@ -1,2 +1,7 @@
 #Asylbekova Aiymgul Func_Prog Tue 8:55
 print("Asylbekova Aiymgul Func_Prog Tue 8:55")
+a=int(input("Туған жылыңыз: "))
+if a==2003:
+    print("Cіз дұрыс енгіздіңіз!")
+else:
+    print("Қате енгіздіңіз!")
